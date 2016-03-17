88111bfeaf068e603259d8c13976af08a1b092a9
Mónica Rodrigues <monica85rodrigues@gmail.com>
2016-03-17 21:57:47 +0000

    First commit with presentation and demo.

---
+++ NetpontoSPADemo/NetpontoSPA/scripts/app/controllers/app-attendance-controller.js
@@ -0,0 +1,49 @@
+﻿(function() {
+
+    function AttendanceController($scope, $http, attendanceService) {
+
+        $scope.filters = {};
+
+        $scope.checkIn = function (user) {
+            user.IsPresent = !user.IsPresent;
+        };
+
+        $scope.goLunch = function (user) {
+            user.IsLunch = !user.IsLunch;
+        };
+
+        $scope.getUsersGoingOrNotGoingLunch = function(value) {
+            $scope.filters.IsLunch = value;
+        };
+
+        $scope.getUsersIsOrNotPresent = function (value) {
+            $scope.filters.IsPresent = value;
+        };
+
+        $scope.resetFilters = function() {
+            $scope.filters = {};
+        };
+
+        $scope.resetAll = function () {
+            angular.forEach($scope.users, function(usr) {
+                usr.IsLunch = false;
+                usr.IsPresent = false;
+            });
+        };
+
+        var onUsersCompleted = function (data) {
+            $scope.users = data;
+        };
+
+        var onError = function(reason) {
+            $scope.error = "Could not fetch the data";
+        };
+
+        attendanceService.getAttendances().then(onUsersCompleted, onError);
+     
+    }
+
+    var module = angular.module("attendanceManagerApp");
+    module.controller("AttendanceController", ["$scope", "$http","attendanceService", AttendanceController]);
+
+}());
---
+++ NetpontoSPADemo/NetpontoSPA/scripts/app/services/app-attendance-service.js
@@ -0,0 +1,24 @@
+﻿(function() {
+
+    var AttendanceService = function($http) {
+
+        var urls = {
+            attendances: "/scripts/data/users.json"
+        };
+
+        var getAttendances = function() {
+            return $http.get(urls.attendances).then(function(response) {
+                return response.data;
+            });
+        };
+
+        return {
+            getAttendances: getAttendances
+        };
+
+    };
+
+    angular.module("attendanceManagerApp").factory("attendanceService", ["$http", AttendanceService]);
+
+
+}());
